5d9d8eaf1d06a45296efe4e243249737b4f77854
svbolotaew@outlook.com <svbolotaew@outlook.com>
2020-10-10 18:55:07 +0300

    Добавьте файлы проекта.

---
+++ WebAppMVC/Startup.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebAppMVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // если приложение в процессе разработки
+            if (env.IsDevelopment())
+            {
+                // то выводим информацию об ошибке, при наличии ошибки
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            //перенаправляет все запросы HTTP на HTTPS
+            app.UseHttpsRedirection();
+
+            //предоставляет поддержку обработки статических файлов
+            app.UseStaticFiles();
+
+            // добавляем возможности маршрутизации
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            // устанавливаем маршруты, которые будут обрабатываться
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapGet("slay", async context =>
+                {
+                    await context.Response.WriteAsync("Слава изучает ASP");
+                });
+            });
+        }
+    }
+}
